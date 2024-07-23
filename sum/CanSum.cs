50f94d9039b7a417f5afc185808d097b19c55a24
Ray <raytsang21@gmail.com>
2024-07-23 11:23:36 -0700

    2nd update CanSum.cs

--- sum/CanSum.cs
+++ sum/CanSum.cs
@@ -8,9 +8,30 @@ namespace DynamicProgramming.Sum
 {
     public class CanSum : ICanSum
     {
-        public int Sum(int[] num, int target)
+        public bool canSum(int targetSum, int[] arrayNum, Dictionary<int, bool> memo)
         {
-            throw new NotImplementedException();
+            //memoization
+            if (memo.ContainsKey(targetSum))
+                return memo[targetSum];
+            //base cases
+            if (targetSum == 0)
+                return true;
+            //targetSum is negative number
+            if (targetSum < 0)
+                return false;
+
+            foreach (var item in arrayNum)
+            {
+                int reminder = targetSum - item;
+                //if either one of child is true, it will return true to parent node
+                if (canSum(reminder, arrayNum, memo) == true)
+                {
+                    memo[targetSum] = true;
+                    return true;
+                }
+            }
+            memo[targetSum] = false;
+            return false;
         }
     }
 }
